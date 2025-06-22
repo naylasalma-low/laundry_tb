45fb70d6254dd23dff3e331128c1174bfea60343
naylasalma-low <kurniaputrinaylasalma@gmail.com>
2025-06-22 14:47:48 +0700

    Add files via upload

---
+++ rumah_laundry/about.php
@@ -0,0 +1,25 @@
+<?php require_once('_header.php') ?>
+
+   
+   <div id="about" class="main-content">
+      <div class="container">
+         <div class="baris">
+            <div class="col mt-2">
+               <div class="card">
+                  <div class="card-body">
+                     <div class="card-flex-column">
+                        <div class="about_header">
+                           <img src="<?=url('_assets/img/logo/logo.png')?>" alt="Logo rumah laundry" width="220">
+                           <p>Jasa laundry tercepat dan termurah se-Jakarta timur.</p>
+                        </div>
+                        <div class="about_us">
+                           <h2>- Tentang Kami -</h2>
+                           <p>Fresh & Clean Laundry kini hadir di Jakarta Timur untuk membantu Anda mendapatkan pakaian bersih, wangi, dan rapi tanpa repot. Kami menyediakan layanan laundry kiloan, cuci setrika, dan cuci satuan dengan proses cepat dan hasil maksimal. Dengan peralatan modern dan deterjen berkualitas, kami menjaga kebersihan dan keawetan pakaian Anda. Didukung oleh staf profesional dan berpengalaman, kami memastikan setiap cucian dikerjakan dengan teliti dan higienis.</p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+<?php require_once('_footer.php') ?>
